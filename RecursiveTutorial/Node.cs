db74abe912c08ffbdca77ee3f859c173213fe506
Corne Combrinck <c1.combrinck@gmail.com>
2023-05-29 13:29:40 +0200

    first commit

---
+++ RecursiveTutorial/Node.cs
@@ -0,0 +1,68 @@
+﻿namespace RecursiveTutorial
+{
+    internal class Node
+    {
+        public string op;
+        public dynamic val1;
+        public dynamic val2;
+
+        public double DoCalculation(dynamic node)
+        {
+            double answer = 0;
+            double node1Val = 0;
+            double node2Val = 0;
+
+            if (IsNumeric(node.val1))
+            {
+                node1Val = node.val1;
+            }
+            else
+            {
+                node1Val = DoCalculation(node.val1);
+            }
+
+            if (IsNumeric(node.val2))
+            {
+                node2Val = node.val2;
+            }
+            else
+            {
+                node2Val = DoCalculation(node.val2);
+            }
+
+            if (!string.IsNullOrEmpty(node.op))
+            {
+                switch (node.op)
+                {
+                    case "add":
+                        answer = node1Val + node2Val;
+                        break;
+                    case "sub":
+                        answer = node1Val - node2Val;
+                        break;
+                    case "mult":
+                        answer = node1Val * node2Val;
+                        break;
+                    case "div":
+                        answer = node1Val / node2Val;
+                        break;
+                }
+            }
+
+            return answer;
+        }
+
+        public bool IsNumeric(object o)
+        {
+            switch (Type.GetTypeCode(o.GetType()))
+            {
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ RecursiveTutorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using RecursiveTutorial;
+
+
+var node = new Node();
+
+int t4Node1 = 100;
+int t4Node2 = 25;
+int t4Node3 = 75;
+int t4Node4 = 50;
+
+object t3Node1 = new { op = "div", val1 = t4Node1, val2 = t4Node2 };
+object t3Node2 = new { op = "sub", val1 = t4Node3, val2 = t4Node4 };
+
+object t2Node2 = new { op = "mult", val1 = t3Node1, val2 = t3Node2 };
+
+object t1Node1 = new { op = "add", val1 = 5, val2 = t2Node2 };
+
+//Build structure
+
+var a = node.DoCalculation(t1Node1);
+Console.WriteLine(a);
